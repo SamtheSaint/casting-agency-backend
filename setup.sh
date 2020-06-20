adbc63690dfa87f0c698719c357f0ad676b1946c
Samuel Adekunle <ebunsamuel@yahoo.com>
2020-06-20 14:17:26 +0100

    minor changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 export DATABASE_URL="REPLACE_WITH_LOCAL_POSTGRES_URL"
 export FLASK_APP=app.py
 
-export AUTH0_DOMAIN = 'dev-3v5wdr91.eu.auth0.com'
-export API_AUDIENCE = 'agency'
+export AUTH0_DOMAIN='dev-3v5wdr91.eu.auth0.com'
+export API_AUDIENCE='agency'
+
